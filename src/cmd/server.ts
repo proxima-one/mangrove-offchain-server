521403e9f8deabc0442552e9b8b801e8ca5345a7
Peter Kejlberg Madsen <peter@mangrove.exchange>
2022-11-29 16:02:38 +0100

    Added playground option

--- src/cmd/server.ts
+++ src/cmd/server.ts
@@ -11,6 +11,7 @@ import bodyParser from "body-parser";
 import rateLimit from "express-rate-limit";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from '@apollo/server/plugin/landingPage/default';
 import {
   CustomMakerBalanceFieldsResolver,
   CustomMangroveFieldsResolver,
@@ -161,7 +162,15 @@ async function main() {
 
   const server = new ApolloServer({
     schema,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+
+    plugins: [     // Install a landing page plugin based on NODE_ENV
+    process.env.GRAPHQL_PRODUCTION === 'true'
+      ? ApolloServerPluginLandingPageProductionDefault({
+          graphRef: 'my-graph-id@my-graph-variant',
+          footer: false,
+        })
+      : ApolloServerPluginLandingPageLocalDefault({ footer: false })
+      , ApolloServerPluginDrainHttpServer({ httpServer })],
   });
 
   await server.start();
